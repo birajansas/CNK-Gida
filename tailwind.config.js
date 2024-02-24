2f62143ad6358fef3ea7707074db6662d9144601
Safiullah Zinone <birajansas@gmail.com>
2024-02-24 12:18:13 +0300

    uppdate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+
+    extend: {
+      fontFamily: {
+        open: ['Open Sans', 'sans-serif']
+      },
+
+      colors: {
+        'cnk-black': '#000000',
+        'cnk-red': '#E31E24',
+        'cnk-gray': '#D5D5D5'
+      },
+    },
   },
   plugins: [],
 }
